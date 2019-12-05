7f191e42e9eb1c558259e19ca29ce79e5f76df72
Student <example@example.com>
2019-12-04 20:07:00 -0800

    add all scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $("form#blanks").submit(function(event) {
 
-    var shoplift = parseInt($(select#shoplift).val());
+    var shoplift = parseInt($("#shoplift").val());
     var operator = $("input:radio[name=operator]:checked").val();
+    event.preventDefault();
 
-
-  event.preventDefault();
+  if (shoplift === 1||2 && operator === "chocolate") {
+    $("#Dustin").show();
+  } else if (shoplift === 1||2 && operator === "pizza") {
+    $("#Steve").show();
+  } else if (shoplift === 1||2 && operator === "apple") {
+    $("#Nancy").show();
+  } else if (shoplift === 1||2 && operator === "chips") {
+    $("#Eleven").show();
+  } else if (shoplift === 1||2 && operator === "peanut-butter") {
+    $("#Hopper").show();
+  } else if (shoplift === 1 && operator === "bacon") {
+    $("#Joyce").show();
+  }
   });
 });
