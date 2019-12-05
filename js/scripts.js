6afefe7fcb578a21e5a4cb5009ed9b58fdea4a1f
Student <example@example.com>
2019-12-04 19:38:32 -0800

    edit index.html, add script to scripts.js, add radio and option, and submit button.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("form#blanks").submit(function(event) {
+
+    var shoplift = parseInt($(select#shoplift).val());
+    var operator = $("input:radio[name=operator]:checked").val();
+
+
+  event.preventDefault();
+  });
+});
